a02889318185965f6d31c7af879fb15f14758679
poiler22 <bright_ra2@hotmail.com>
2018-10-22 22:12:41 +0700

    Update CozmoMainCode.py

--- CozmoMainCode.py
+++ CozmoMainCode.py
@@ -13,6 +13,9 @@ import webbrowser
 import smtplib
 import requests
 import random
+import functionsCozmo
+from weather import Weather
+
 
 APIKEY = "CC8o4IwXba1_aD74BgI46ZHxFBQ"
 
@@ -73,159 +76,14 @@ def initBot(apiKey):
 
     return bot
 
-
-def songright(robot: cozmo.robot.Robot):
-    # scales is a list of the words for Cozmo to sing
-    scales = ["Doe", "Ray", "Mi", "Fa", "So", "La", "Ti", "Doe"]
-
-    # Find voice_pitch_delta value that will range the pitch from -1 to 1 over all of the scales
-    voice_pitch = -1.0
-    voice_pitch_delta = 20.0 / (len(scales) - 1)
-
-    # Move head and lift down to the bottom, and wait until that's achieved
-    robot.move_head(-5)  # start moving head down so it mostly happens in parallel with lift
-    robot.set_lift_height(0.0).wait_for_completed()
-    robot.set_head_angle(degrees(-25.0)).wait_for_completed()
-
-    # Start slowly raising lift and head
-    robot.move_lift(0.15)
-    robot.move_head(0.15)
-
-    # "Sing" each note of the scale at increasingly high pitch
-    for note in scales:
-        robot.say_text(note, voice_pitch=voice_pitch, duration_scalar=0.3).wait_for_completed()
-        voice_pitch += voice_pitch_delta
-
-
-def cozmo_singing(robot: cozmo.robot.Robot):
-    # Create an array of SongNote objects, consisting of all notes from C2 to C3_Sharp
-    notes = [
-        cozmo.song.SongNote(cozmo.song.NoteTypes.E2, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.C2_Sharp, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.D2, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.D2_Sharp, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.E2, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.F2, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.F2_Sharp, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.G2, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.G2_Sharp, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.A2, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.A2_Sharp, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.B2, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.C3, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.C3_Sharp, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.Rest, cozmo.song.NoteDurations.Quarter)]
-
-    # Play the ascending notes
-    robot.play_song(notes, loop_count=1).wait_for_completed()
-
-    # Create an array of SongNote objects, consisting of the C3 pitch with varying durations
-    notes = [
-        cozmo.song.SongNote(cozmo.song.NoteTypes.C3, cozmo.song.NoteDurations.Half),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.C3, cozmo.song.NoteDurations.ThreeQuarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.Rest, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.C3, cozmo.song.NoteDurations.Quarter),
-        cozmo.song.SongNote(cozmo.song.NoteTypes.C3, cozmo.song.NoteDurations.Whole)]
-
-    # Play the notes with varying durations
-    robot.play_song(notes, loop_count=1).wait_for_completed()
-
-
-# The main loop to our program. Runs after all the initialization.
-def follow_faces(robot: cozmo.robot.Robot):
-    '''The core of the follow_faces program'''
-
-    # Move lift down and tilt the head up
-    robot.move_lift(-3)
-    robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE).wait_for_completed()
-    # look_around = robot.start_behavior(cozmo.behavior.BehaviorTypes.LookAroundInPlace)
-    face_to_follow = None
-    print("Press CTRL-C to quit")
-    while True:
-        turn_action = None
-        if face_to_follow:
-            # start turning towards the face
-            turn_action = robot.turn_towards_face(face_to_follow)
-        if not (face_to_follow and face_to_follow.is_visible):
-            # find a visible face, timeout if nothing found after a short while
-            try:
-                face_to_follow = robot.world.wait_for_observed_face(timeout=5)
-            except asyncio.TimeoutError:
-                print("Didn't find a face - exiting!")
-                return
-
-        if turn_action:
-            # Complete the turn action if one was in progress
-            turn_action.wait_for_completed()
-
-        time.sleep(.1)
-
-
-def cozmo_squares(robot: cozmo.robot.Robot):
-    # Use a "for loop" to repeat the indented code 4 times
-    # Note: the _ variable name can be used when you don't need the value
-    for _ in range(4):
-        robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
-        robot.turn_in_place(degrees(90)).wait_for_completed()
-
-
-def running(robot: cozmo.robot.Robot):
-    # Drive forwards for 150 millimeters at 50 millimeters-per-second.
-    robot.drive_straight(distance_mm(150), speed_mmps(50)).wait_for_completed()
-
-    # Turn 90 degrees to the left.
-    # Note: To turn to the right, just use a negative number.
-    robot.turn_in_place(degrees(90)).wait_for_completed()
-
-
-def motion_step(robot: cozmo.robot.Robot):
-    # grab a list of animation triggers
-    all_animation_triggers = robot.anim_triggers
-
-    # randomly shuffle the animations
-    random.shuffle(all_animation_triggers)
-
-    # select the first three animations from the shuffled list
-    triggers = 3
-    chosen_triggers = all_animation_triggers[:triggers]
-    print('Playing {} random animations:'.format(triggers))
-
-    # play the three random animations one after the other, waiting for each to complete
-    for trigger in chosen_triggers:
-        print('Playing {}'.format(trigger.name))
-        robot.play_anim_trigger(trigger).wait_for_completed()
-
-    # grab animation triggers that have 'WinGame' in their name
-    chosen_triggers = [trigger for trigger in robot.anim_triggers if 'WinGame' in trigger.name]
-
-    # play the three random animations one after the other, waiting for each to complete
-    for trigger in chosen_triggers:
-        print('Playing {}'.format(trigger.name))
-        robot.play_anim_trigger(trigger).wait_for_completed()
-
-
-def cozmo_nod(robot: cozmo.robot.Robot):
-    robot.move_head(-5).wait_for_completed
-    robot.move_head(10).wait_for_completed
-    robot.move_head(-5).wait_for_completed
-
-
-def cozmo_refuse(robot: cozmo.robot.Robot):
-    robot.turn_in_place(degrees(45)).wait_for_completed
-    robot.turn_in_place(degrees(-90)).wait_for_completed
-    robot.turn_in_place(degrees(45)).wait_for_completed
-
-
-def cozmo_spin(robot: cozmo.robot.Robot):
-    robot.turn_in_place(degrees(360)).wait_for_completed
-
-
 def mainLoop(robot: cozmo.robot.Robot):
     while True:
         # In a loop, we grab the user input
         print("Listening...")
         humanString = (voiceParse.parseVoice()).lower()
         ListOfCommand = [str(s) for s in (humanString.lower()).split()]
+
+
         # Check it for a quit condition.
         if humanString.lower() == "quit":
             # If we quit, we log the quit and leave the program.
@@ -247,6 +105,7 @@ def mainLoop(robot: cozmo.robot.Robot):
             robot.say_text("Done").wait_for_completed()
             continue
 
+
         if 'open google' in humanString:
             reg_ex = re.search('open google(.*)', humanString.lower())
             url = 'https://www.google.com/'
@@ -261,6 +120,32 @@ def mainLoop(robot: cozmo.robot.Robot):
             robot.say_text("Done").wait_for_completed()
             continue
 
+        if 'open my favorite song' in humanString:
+            robot.say_text("What song?").wait_for_completed()
+            humanString1 = (voiceParse.parseVoice()).lower()
+            print("Human says: " + humanString1)
+            reg_ex = re.search('(.+)', humanString1)
+
+            if reg_ex:
+                SongName = reg_ex.group(1)
+                ListofSongs = {"part of your world": "watch?v=gtpLsPPtC88", "full nocturne": 'watch?v=liTSRH4fix4',
+                               'eyes on fire': 'watch?v=LAxCqlU-OAo', 'nocturne in f minor': 'watch?v=E3qHO9aOQYM',
+                               'moonlight sonata': 'watch?v=4Tr0otuiQuU', 'clair de lune': 'watch?v=ea2WoUtbzuw',
+                               'hello': 'watch?v=YQHsXMglC9A', 'skyfall': 'watch?v=DeumyOzKqgI'}
+
+                subUrl = 'https://www.youtube.com/search?q=' + SongName
+                if SongName not in ListofSongs:
+                    webbrowser.open(subUrl)
+                else:
+                    url = 'https://www.youtube.com/' + str(ListofSongs.get(SongName))
+                    webbrowser.open(url)
+            elif type(SongName) == type(None):
+                robot.say_text("No command found").wait_for_completed()
+                continue
+            robot.say_text("Done").wait_for_completed()
+            addEntry(log, "Cozmo says: " + "Done")
+            print("Cozmo says: " + "Done")
+            continue
 
 
 
@@ -306,7 +191,7 @@ def mainLoop(robot: cozmo.robot.Robot):
             print("Human says: " + recipient)
             #you can add any email in the line below
             ListofEmails = {"bright":"bright_ra2@hotmail.com","boss":"biggerbosssuper@gmail.com",
-                            "tim":"tim.dettmar@gmail.com"}
+                            "tim":"tim.dettmar@gmail.com","lucky":"vorachat1239@gmail.com"}
             if recipient in ListofEmails:
                 robot.say_text("What should I say?").wait_for_completed()
                 addEntry(log, "Cozmo says: " + "What should I say?")
@@ -343,6 +228,27 @@ def mainLoop(robot: cozmo.robot.Robot):
                 print("Cozmo says: " + "I don\'t know him")
             continue
 
+
+        if 'weather forecast in' in humanString:
+            addEntry(log, "Human says: " + humanString)
+            print("Human says: " + humanString)
+            reg_ex = re.search('weather forecast in (.*)', humanString)
+            if reg_ex:
+                city = reg_ex.group(1)
+                weather = Weather()
+                location = weather.lookup_by_location(city)
+                forecasts = location.forecast
+                for i in range(0, 3):
+                    TempetureForecast = ('On %s will it %s. The maximum temperture will be %.1f degrees Celcius.'
+                             'The lowest temperature will be %.1f degrees Celcius.' % (
+                             forecasts[i].date, forecasts[i].text, int(forecasts[i].high),
+                             int(forecasts[i].low)))
+                robot.say_text(TempetureForecast).wait_for_completed()
+                addEntry(log, "Cozmo says: " + TempetureForecast)
+                print("Cozmo says: " + TempetureForecast)
+                continue
+
+
         if ({"cosmo", "joke"} <= set(ListOfCommand)) or ({"cozmo", "joke"} <= set(ListOfCommand)):
             ListOfCommand.clear()
             res = requests.get(
@@ -360,6 +266,25 @@ def mainLoop(robot: cozmo.robot.Robot):
                 addEntry(log, "Cozmo says: " + 'oops!I ran out of jokes')
             continue
 
+        #Action Commands
+
+        if ({"sing", "song", "cosmo","stupid"} <= set(ListOfCommand)) or ({"sing", "song", "cozmo","stupid"} <= set(ListOfCommand)):
+            addEntry(log, "Human says: " + humanString)
+            print("Human says: " + humanString)
+            ListOfCommand.clear()
+            functionsCozmo.cozmo_singing(robot)
+            robot.say_text("Do you like my song?").wait_for_completed()
+            print("Cozmo says: " + "Do you like my song?")
+            addEntry(log, "Cozmo says: " + "Do you like my song?")
+            continue
+
+        if ({"angry", "cosmo"} <= set(ListOfCommand)) or ({"cosmo", "mad"} <= set(ListOfCommand)) or (
+                {"cozmo", "mad"} <= set(ListOfCommand)) or ({"cozmo", "angry"} <= set(ListOfCommand)):
+            ListOfCommand.clear()
+            functionsCozmo.motion_step(robot)
+
+
+
         # Else, we log what the human said.
         addEntry(log, "Human says: " + humanString)
         print("Human says: " + humanString)
@@ -373,6 +298,7 @@ def mainLoop(robot: cozmo.robot.Robot):
 
         # Then we make Cozmo say it.
         robot.say_text(cozmoString).wait_for_completed()
+        ListOfCommand.clear()
 
 
 # This is where our code begins. We can first initialize everything,
